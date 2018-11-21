01f408b416dcbaf4188702261cd1f1f18790b91c
Sagar Rathna Sabapathy <33907389+sabapathy-sagar@users.noreply.github.com>
2018-11-21 07:41:03 +0100

    setup the model for the app

--- src/Model.js
+++ src/Model.js
@@ -1,3 +1,9 @@
-const initModel = {};
+const initModel = {
+  leftValue: 0,
+  leftUnit: "Celsius",
+  rightValue: 0,
+  rightUnit: "Fahrenheit",
+  sourceLeft: true
+};
 
 export default initModel;
